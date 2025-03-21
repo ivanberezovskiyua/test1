9e9a35061d4eb39149dbbc7009cf86282fd2db8f
ivanberezovskiyua <berez.iv.ua@gmail.com>
2025-03-21 22:02:41 +0200

    Update PlayerMovement.cs

--- My project/Assets/New Folder/PlayerMovement.cs
+++ My project/Assets/New Folder/PlayerMovement.cs
@@ -1,8 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    float speed;
+    float maxspeed;
+
+    Rigidbody2D rigitbody2D;
+
+    private void Start()
+    {
+        rigitbody2D = GetComponent<Rigidbody2D>();
+        speed = 10;
+    }
+
+    private void FixedUpdate()
+    {
+        Walk();
+        
+    }
+
+    private void Walk()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigitbody2D.AddForce(Vector2.left * speed);
+            rigitbody2D.AddForce(Vector2.right * speed);
+            
+        }
+        else if (Input.GetKey(KeyCode.D))
+            rigitbody2D.AddForce(Vector2.right * speed);  
+    }  
     
+    
+
+
 }
