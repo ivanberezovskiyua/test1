3b4af2150450ad8a5920406bb69cb2773df83e53
ivanberezovskiyua <berez.iv.ua@gmail.com>
2025-03-23 18:21:56 +0200

    asd

--- My project/Assets/New Folder/PlayerMovement.cs
+++ My project/Assets/New Folder/PlayerMovement.cs
@@ -6,35 +6,69 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     float speed;
-    float maxspeed;
 
     Rigidbody2D rigitbody2D;
-
+    private Vector3  moveDirection;
+    [SerializeField] private float maxspeed;
+    [SerializeField] private float jumpForce;
+    
     private void Start()
     {
         rigitbody2D = GetComponent<Rigidbody2D>();
         speed = 10;
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         Walk();
-        
+        Jump();
     }
+    
 
     private void Walk()
     {
-        if (Input.GetKey(KeyCode.A))
+        moveDirection= Vector3.zero;
+
+        speed = maxspeed;
+        
+       if (Input.GetKey(KeyCode.A))
+       {
+           moveDirection += Vector3.left;
+           
+       }
+       else if (Input.GetKey(KeyCode.D))
+       {
+           moveDirection += Vector3.right;  
+       }
+
+       if (Input.GetKey(KeyCode.LeftShift))
+       {
+           speed *= 2;
+       }
+       
+       
+       moveDirection *= speed;
+       moveDirection.y = rigitbody2D.velocity.y;
+       rigitbody2D.velocity = moveDirection;
+
+
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rigitbody2D.AddForce(Vector2.left * speed);
-            rigitbody2D.AddForce(Vector2.right * speed);
-            
+            rigitbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
-        else if (Input.GetKey(KeyCode.D))
-            rigitbody2D.AddForce(Vector2.right * speed);  
-    }  
-    
-    
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       if (collision.gameObject.TryGetComponent <Ground>(out Ground  playerMovement))
+    }
+
+
+
 
 
 }
